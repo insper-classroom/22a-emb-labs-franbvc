10402538ea336e43a8e85ef114cd8fe66f03604a
franbvc <franbvc@gmail.com>
2022-02-24 09:11:27 -0300

    LAB 1 funcioando

--- 1-PIO-IO/src/main.c
+++ 1-PIO-IO/src/main.c
@@ -23,10 +23,48 @@
 /* defines                                                              */
 /************************************************************************/
 
-#define LED_PIO           
-#define LED_PIO_ID        
-#define LED_PIO_IDX       
-#define LED_PIO_IDX_MASK  
+#define LED_PIO           PIOC                 // periferico que controla o LED
+#define LED_PIO_ID        ID_PIOC              // ID do periférico PIOC (controla LED)
+#define LED_PIO_IDX       8                    // ID do LED no PIO
+#define LED_PIO_IDX_MASK  (1u << LED_PIO_IDX)   // Mascara para CONTROLARMOS o LED  
+
+// Configuracoes do botao
+#define BUT_PIO			 PIOA
+#define BUT_PIO_ID		 ID_PIOA
+#define BUT_PIO_IDX		 11
+#define BUT_PIO_IDX_MASK (1u << BUT_PIO_IDX) // esse já está pronto.
+
+// Config OLED1 LEDs
+#define LED1_PIO           PIOA
+#define LED1_PIO_ID        ID_PIOA
+#define LED1_PIO_IDX       0
+#define LED1_PIO_IDX_MASK  (1u << LED1_PIO_IDX)
+
+#define LED2_PIO           PIOC
+#define LED2_PIO_ID        ID_PIOC
+#define LED2_PIO_IDX       30
+#define LED2_PIO_IDX_MASK  (1u << LED2_PIO_IDX)
+
+#define LED3_PIO           PIOB
+#define LED3_PIO_ID        ID_PIOB
+#define LED3_PIO_IDX       2
+#define LED3_PIO_IDX_MASK  (1u << LED3_PIO_IDX)
+
+// Config OLED1 Buttons
+#define BUT1_PIO		  PIOD
+#define BUT1_PIO_ID		  ID_PIOD
+#define BUT1_PIO_IDX	  28
+#define BUT1_PIO_IDX_MASK (1u << BUT1_PIO_IDX)
+
+#define BUT2_PIO		  PIOC
+#define BUT2_PIO_ID		  ID_PIOC
+#define BUT2_PIO_IDX	  31
+#define BUT2_PIO_IDX_MASK (1u << BUT2_PIO_IDX)
+
+#define BUT3_PIO		  PIOA
+#define BUT3_PIO_ID		  ID_PIOA
+#define BUT3_PIO_IDX	  19
+#define BUT3_PIO_IDX_MASK (1u << BUT3_PIO_IDX)
 
 /************************************************************************/
 /* constants                                                            */
@@ -51,9 +89,45 @@ void init(void);
 /************************************************************************/
 
 // Função de inicialização do uC
-void init(void)
-{
+void init(void){
+	// Initialize the board clock
+	sysclk_init();
 
+	// Desativa WatchDog Timer
+	WDT->WDT_MR = WDT_MR_WDDIS;
+	
+	// Ativa o PIO na qual o LED foi conectado
+	// para que possamos controlar o LED.
+	pmc_enable_periph_clk(LED_PIO_ID);
+	pmc_enable_periph_clk(LED1_PIO_ID);
+	pmc_enable_periph_clk(LED2_PIO_ID);
+	pmc_enable_periph_clk(LED3_PIO_ID);
+	
+	//Inicializa saídas
+	pio_set_output(LED_PIO, LED_PIO_IDX_MASK, 0, 0, 0);
+	pio_set_output(LED1_PIO, LED1_PIO_IDX_MASK, 0, 0, 0);
+	pio_set_output(LED2_PIO, LED2_PIO_IDX_MASK, 0, 0, 0);
+	pio_set_output(LED3_PIO, LED3_PIO_IDX_MASK, 0, 0, 0);
+	
+	
+	// Inicializa PIO do botao
+	pmc_enable_periph_clk(BUT_PIO_ID);
+	pmc_enable_periph_clk(BUT1_PIO_ID);
+	pmc_enable_periph_clk(BUT2_PIO_ID);
+	pmc_enable_periph_clk(BUT3_PIO_ID);
+	
+	// configura pino ligado ao botão como entrada com um pull-up.
+	pio_set_input(BUT_PIO, BUT_PIO_IDX_MASK, PIO_DEFAULT);
+	pio_set_input(BUT1_PIO, BUT1_PIO_IDX_MASK, PIO_DEFAULT);
+	pio_set_input(BUT2_PIO, BUT2_PIO_IDX_MASK, PIO_DEFAULT);
+	pio_set_input(BUT3_PIO, BUT3_PIO_IDX_MASK, PIO_DEFAULT);
+	
+	// faz com que o valor padrão do pino seja o energizado
+	pio_pull_up(BUT_PIO, BUT_PIO_IDX_MASK, 1);
+	pio_pull_up(BUT1_PIO, BUT1_PIO_IDX_MASK, 1);
+	pio_pull_up(BUT2_PIO, BUT2_PIO_IDX_MASK, 1);
+	pio_pull_up(BUT3_PIO, BUT3_PIO_IDX_MASK, 1);
+	
 }
 
 /************************************************************************/
@@ -69,6 +143,53 @@ int main(void)
   // aplicacoes embarcadas não devem sair do while(1).
   while (1)
   {
+	if (!pio_get(BUT_PIO, PIO_INPUT, BUT_PIO_IDX_MASK)) {
+		for (int i = 0; i < 25; i++) {
+					pio_set(LED_PIO, LED_PIO_IDX_MASK);
+					delay_ms(100);
+					pio_clear(LED_PIO, LED_PIO_IDX_MASK);
+					delay_ms(100);
+		}
+	}
+	else {
+		pio_set(LED_PIO, LED_PIO_IDX_MASK);
+	}
+	
+	if (!pio_get(BUT1_PIO, PIO_INPUT, BUT1_PIO_IDX_MASK)) {
+		for (int i = 0; i < 25; i++) {
+			pio_set(LED1_PIO, LED1_PIO_IDX_MASK);
+			delay_ms(100);
+			pio_clear(LED1_PIO, LED1_PIO_IDX_MASK);
+			delay_ms(100);
+		}
+	}
+	else {
+		pio_set(LED1_PIO, LED1_PIO_IDX_MASK);
+	}
+	
+	if (!pio_get(BUT2_PIO, PIO_INPUT, BUT2_PIO_IDX_MASK)) {
+		for (int i = 0; i < 25; i++) {
+			pio_set(LED2_PIO, LED2_PIO_IDX_MASK);
+			delay_ms(100);
+			pio_clear(LED2_PIO, LED2_PIO_IDX_MASK);
+			delay_ms(100);
+		}
+	}
+	else {
+		pio_set(LED2_PIO, LED2_PIO_IDX_MASK);
+	}
+	
+	if (!pio_get(BUT3_PIO, PIO_INPUT, BUT3_PIO_IDX_MASK)) {
+		for (int i = 0; i < 25; i++) {
+			pio_set(LED3_PIO, LED3_PIO_IDX_MASK);
+			delay_ms(100);
+			pio_clear(LED3_PIO, LED3_PIO_IDX_MASK);
+			delay_ms(100);
+		}
+	}
+	else {
+		pio_set(LED3_PIO, LED3_PIO_IDX_MASK);
+	}
 
   }
   return 0;
